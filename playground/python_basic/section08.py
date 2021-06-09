45d4237e85fc993025b9324757c284fcb3788c27
Hyukho Song <hyukho83@gmail.com>
2021-06-10 08:29:22 +0900

    updated

--- playground/python_basic/section08.py
+++ playground/python_basic/section08.py
@@ -6,21 +6,53 @@
 # .. : 부모 디렉토리
 # .  : 현재 디렉토리
 
-class Fibonacci:
-    def __init__(self, title="fibonacci"):
-        self.title = title
-        
-    def fib(n):
-        a, b = 0, 1
-        while a < n:
-            print(a, end='')
-            a, b = b, a + b
-        print()
-        
-    def fib2(n):
-        result = []
-        a, b = 0, 1
-        while a < n:
-            result.append(a)
-            a, b = b, a + b
-        return result
+# 사용1(클래스)
+
+from pkg.fibonacci import Fibonacci
+
+Fibonacci.fib(300)
+# 0 1 1 2 3 5 8 13 21 34 55 89 144 233
+
+print("ex2 : ", Fibonacci.fib2(400))
+# ex2 :  [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377]
+
+# 사용2(클래스) # 권장하지 않음
+
+from pkg.fibonacci import *
+Fibonacci.fib(500)
+
+print("ex2 : ", Fibonacci.fib2(600))
+# ex2 :  [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377]
+
+print("ex2 : ", Fibonacci().title)
+# ex2 :  fibonacci
+
+# 사용3(클래스)
+from pkg.fibonacci import Fibonacci as fb
+
+fb.fib(1000)
+# 0 1 1 2 3 5 8 13 21 34 55 89 144 233 377 610 987
+print("ex3 : ", fb.fib2(1600))
+# ex3 :  [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597]
+print("ex3 : ", fb().title)
+# ex3 :  fibonacci
+
+# 사용4(함수)
+import pkg.calculations as c
+
+print("ex4 : ", c.add(10, 100))
+# ex4 :  110
+print("ex4 : ", c.mul(10, 100))
+# ex4 :  1000
+
+# 사용5(함수)
+from pkg.calculations import div as d
+print("ex5 : ", int(d(100,10)))
+# ex5 :  10
+
+# 사용6
+import pkg.prints as p
+p.prt1()
+# I'm Niceboy!
+p.prt2()
+# I'm Goodboy!
